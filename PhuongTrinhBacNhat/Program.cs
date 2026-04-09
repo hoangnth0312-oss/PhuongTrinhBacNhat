d746ff70b3ac60f002230294229404f2ca46271f
huyhoang <hoangnth0312@gmail.com>
2026-04-09 09:55:25 +0700

    update

--- PhuongTrinhBacNhat/Program.cs
+++ PhuongTrinhBacNhat/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Linear Equation Resolver");
+Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+
+
+Console.WriteLine("nhap a: ");
+Double a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("nhap b: ");
+Double b = Convert.ToDouble(Console.ReadLine());
+
+double x = (-b)/a;
+
+if (a == 0)
+{
+    if (b == 0)
+    {
+        Console.WriteLine("Phuong trinh vo so nghiem!");
+    }
+    else
+    {
+        Console.WriteLine("Phuong trinh vo nghiem!");
+    }
+}
+else
+{
+    Console.WriteLine("Phuong trinh co nghiem x: " + x);
+}
